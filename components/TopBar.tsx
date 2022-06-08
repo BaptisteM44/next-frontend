6199ec66b073b69e048aca7e0ce6d725e03ba52d
Thomas Cazade <cazade.thomas@gmail.com>
2022-06-08 17:34:08 +0200

    Update TopBar wording

--- components/TopBar.tsx
+++ components/TopBar.tsx
@@ -1,7 +1,16 @@
 const TopBar: React.FC = () => (
   <aside className="w-full bg-accent py-3">
     <p className="text-center text-[13px] font-normal leading-none text-white">
-      Free express shipping ➺ On all orders $40 + Easy returns
+      This is a demo store for demonstration purposes only. Content is the
+      property of{" "}
+      <a
+        href="https://pistilsnursery.com/"
+        className="underline"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Pistils Nursery
+      </a>
     </p>
   </aside>
 );
