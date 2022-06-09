de769dacc76061e26350c4a8eaf9843a6a029f3b
Thomas Cazade <cazade.thomas@gmail.com>
2022-06-09 14:39:49 +0200

    Add missing snipcart-add-item on ProductCta

--- components/product/ProductCta.tsx
+++ components/product/ProductCta.tsx
@@ -20,7 +20,7 @@ const ProductCta: React.FC<ProductCtaProps> = ({
   return (
     <button
       type="button"
-      className="rounded-full bg-accent px-6 py-3 text-sm font-medium uppercase tracking-wide text-tertiary transition-opacity duration-200 ease-in-out hover:bg-opacity-80"
+      className="snipcart-add-item rounded-full bg-accent px-6 py-3 text-sm font-medium uppercase tracking-wide text-tertiary transition-opacity duration-200 ease-in-out hover:bg-opacity-80"
       data-item-id={id}
       data-item-name={name}
       data-item-price={price}
